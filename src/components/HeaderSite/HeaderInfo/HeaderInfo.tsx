ba08dbc493ca9a6b077c24c6a638b6287240e4e9
Bakirovildar <bakirovildar2011@mail.ru>
2023-01-16 21:58:52 +0500

    Добавил header main и добавил логотип сайта в отдельный компонент

--- src/components/HeaderSite/HeaderInfo/HeaderInfo.tsx
+++ src/components/HeaderSite/HeaderInfo/HeaderInfo.tsx
@@ -5,9 +5,11 @@ import {NavInfo} from "./NavInfo";
 
 export function HeaderInfo() {
     const arr = [
-        {title: 'adawd', id: 2},
-        {title: 'adawdqwdwq', id: 3},
-        {title: 'adaswww', id: 4},
+        {title: 'Бренды', id: 2},
+        {title: 'Доставка', id: 3},
+        {title: 'Возврат', id: 4},
+        {title: 'Документация', id: 5},
+        {title: 'Контакты', id: 6},
     ]
 
   return (
---
+++ src/components/HeaderSite/HeaderMain/HeaderMain.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './headermain.css';
+import {LogoHeader} from "./LogoHeader";
+
+export function HeaderMain() {
+  return (
+      <div>
+          <LogoHeader />
+      </div>
+  );
+}
