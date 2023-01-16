64c2f3dd471f5755473e55898c2679e3c678bd22
Bakirovildar <bakirovildar2011@mail.ru>
2023-01-16 15:38:52 +0500

    Добавил header info

---
+++ src/types/interface.ts
@@ -0,0 +1,12 @@
+export interface ILocationCity {
+    city: string
+}
+
+export interface INavInfo {
+    navTitle: Array<IInfoItem>
+}
+
+export interface IInfoItem {
+    title: string
+    id: number
+}
