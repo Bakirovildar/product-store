f5768b64f059cd00414e3b77306b6d60e6351514
Bakirovildar <bakirovildar2011@mail.ru>
2023-01-17 12:28:20 +0500

    добавил SearchComponent.tsx

---
+++ src/shared/components/SearchComponent/SearchComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './searchcomponent.css';
+
+export function SearchComponent() {
+  return (
+      <input
+          type="text"
+          className='serachComponent'
+      />
+  );
+}
--- src/shared/content/HeaderSite/HeaderMain/HeaderOptions/HeaderOptions.tsx
+++ src/shared/content/HeaderSite/HeaderMain/HeaderOptions/HeaderOptions.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './headeroptions.css'
 import {ButtonComponent} from "../../../../components/ButtonComponent";
+import {SearchComponent} from "../../../../components/SearchComponent";
 
 export function HeaderOptions() {
   return (
       <div>
           <ButtonComponent title={'Каталог'}/>
+          <SearchComponent/>
       </div>
   )
 }
