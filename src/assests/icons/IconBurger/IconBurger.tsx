2537e7949d4dd788a3e0bb94fbbf380d5cf6ce0a
Bakirovildar <bakirovildar2011@mail.ru>
2023-01-17 11:55:47 +0500

    добавил иконку бургера для button

---
+++ src/assests/icons/IconBurger/IconBurger.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function IconBurger() {
+  return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
+          <path d="M4.5 18L14.5 18" stroke="#2F323A" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          <path d="M4.5 12L20.5 12" stroke="#2F323A" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          <path d="M4.5 6L10.5 6" stroke="#2F323A" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+      </svg>
+  );
+}
