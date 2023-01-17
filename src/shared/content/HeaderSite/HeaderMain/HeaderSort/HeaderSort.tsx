3ef4cb56292a4cb06f80cab20f2529a1319b4007
Bakirovildar <bakirovildar2011@mail.ru>
2023-01-17 17:21:34 +0500

    добавил стили для headerSort

--- src/shared/content/HeaderSite/HeaderMain/HeaderSort/HeaderSort.tsx
+++ src/shared/content/HeaderSite/HeaderMain/HeaderSort/HeaderSort.tsx
@@ -3,9 +3,21 @@ import './headersort.css'
 
 export function HeaderSort() {
 
-  return (
-      <div className='headerSort'>
+    const sortItems = [
+        {id: 1, title: 'Акции'},
+        {id: 2, title: 'Строительные материалы'},
+        {id: 3, title: 'Керамическая плитка'},
+        {id: 4, title: 'Краски'},
+        {id: 5, title: 'Сантехника'},
+        {id: 6, title: 'Напольные покрытия'},
+        {id: 7, title: 'Инструменты'},
+        {id: 8, title: 'Обои'},
+        {id: 9, title: 'Окна'},
+    ]
 
-      </div>
-  )
+    return (
+        <div className='headerSort'>
+            {sortItems.map(item => <span>{item.title}</span>)}
+        </div>
+    )
 }
--- src/shared/content/HeaderSite/HeaderSite.tsx
+++ src/shared/content/HeaderSite/HeaderSite.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './headersite.css';
 import {HeaderInfo} from "./HeaderInfo";
 import {HeaderMain} from "./HeaderMain";
+import {HeaderSort} from "./HeaderMain/HeaderSort";
 
 export function HeaderSite() {
-  return (
-      <header className='headerContainer'>
-          <HeaderInfo/>
-          <HeaderMain/>
-      </header>
-  );
+    return (
+        <header className='headerContainer'>
+            <HeaderInfo/>
+            <HeaderMain/>
+            <HeaderSort/>
+        </header>
+    );
 }
