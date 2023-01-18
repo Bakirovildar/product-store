30d226d1eae6b0072dadc8601a52242f1ced2662
Bakirovildar <bakirovildar2011@mail.ru>
2023-01-18 15:29:34 +0500

    изменил стили иконок бургера

--- src/assests/icons/IconBurger/IconBurger.tsx
+++ src/assests/icons/IconBurger/IconBurger.tsx
@@ -5,6 +5,7 @@ export function IconBurger({isWhiteColor, clickBurger}: IIconBurger) {
   return (
       <svg
           onClick={clickBurger}
+          style={{cursor: 'pointer'}}
           xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
           <path d="M4.5 18L14.5 18" stroke={isWhiteColor ? "#fff" : "#2F323A"} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
           <path d="M4.5 12L20.5 12" stroke={isWhiteColor ? "#fff" : "#2F323A"} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
--- src/assests/icons/IconClose/IconClose.tsx
+++ src/assests/icons/IconClose/IconClose.tsx
@@ -5,6 +5,7 @@ export function IconClose({clickBurger}: IIconBurger) {
   return (
       <svg
           onClick={clickBurger}
+          style={{cursor: 'pointer', marginTop: '10px'}}
           xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
           <line x1="16.9476" y1="1.59835" x2="2.09835" y2="16.4476" stroke="#2F323A" strokeWidth="2" strokeLinecap="round"/>
           <line x1="1" y1="-1" x2="22" y2="-1" transform="matrix(-0.707107 -0.707107 -0.707107 0.707107 17.3159 17.8159)" stroke="#2F323A" strokeWidth="2" strokeLinecap="round"/>
