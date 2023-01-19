33e1cc2bc915b9f9dacfc5ff2c836c82e9883aa4
Bakirovildar <bakirovildar2011@mail.ru>
2023-01-19 10:46:46 +0500

    добавил папку helpers и файл sortItems.ts

--- src/shared/content/HeaderSite/HeaderDesktop/HeaderSort/HeaderSort.tsx
+++ src/shared/content/HeaderSite/HeaderDesktop/HeaderSort/HeaderSort.tsx
@@ -1,20 +1,8 @@
 import React from 'react'
 import './headersort.css'
+import {sortItems} from "../../../../../helpers/sortItems";
 
 export function HeaderSort() {
-
-    const sortItems = [
-        {id: 1, title: 'Акции'},
-        {id: 2, title: 'Строительные материалы'},
-        {id: 3, title: 'Керамическая плитка'},
-        {id: 4, title: 'Краски'},
-        {id: 5, title: 'Сантехника'},
-        {id: 6, title: 'Напольные покрытия'},
-        {id: 7, title: 'Инструменты'},
-        {id: 8, title: 'Обои'},
-        {id: 9, title: 'Окна'},
-    ]
-
     return (
         <div className='headerSort'>
             {sortItems.map(item => <span key={item.id}>{item.title}</span>)}
