30e3977ef311686e161c0be2838bf47df2f98fa1
Bakirovildar <bakirovildar2011@mail.ru>
2023-01-19 10:43:07 +0500

    рефакторинг DropdownMenu.tsx

--- src/shared/components/DropdownMenu/DropdownMenu.tsx
+++ src/shared/components/DropdownMenu/DropdownMenu.tsx
@@ -3,12 +3,19 @@ import './dropdownmenu.css';
 import {ButtonComponent} from "../ButtonComponent";
 import {ButtonProfile} from "../ButtonProfile";
 import {IconProfile} from "../../../assests/icons/IconProfile";
+import {IconOrder} from "../../../assests/icons/IconOrder";
+import {LocationInfo} from "../../content/HeaderSite/HeaderDesktop/HeaderInfo/LocationInfo";
 
 export function DropdownMenu() {
   return (
       <div className='dropdownMenu'>
           <ButtonComponent title='Каталог' />
           <ButtonProfile title={'Профиль'} icon={<IconProfile/>}/>
+          <ButtonProfile title={'Заказы'} icon={<IconOrder/>}/>
+          <div className='dropdownCity'>
+              <LocationInfo city={'Москва'}/>
+          </div>
+          
       </div>
   );
 }
